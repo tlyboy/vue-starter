21322e52e1f122773f6b5bddcc12052c367579b8
Guany <1084072690@qq.com>
2023-10-12 09:35:26 +0800

    chore: update

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import '@unocss/reset/tailwind.css'
 import './assets/styles/main.css'
 import './assets/styles/nprogress.css'
 import 'uno.css'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 const pinia = createPinia()
+
+pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
 app.use(router)

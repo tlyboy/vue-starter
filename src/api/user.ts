21cfb606137ff58a6d40dbb52b8134e6af0a85d1
Guany <1084072690@qq.com>
2023-10-30 09:49:53 +0800

    fix: fix params types

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,4 +1,4 @@
-import type { FetchOptions } from 'ofetch'
+import type { SearchParameters } from 'ofetch'
 import type { UserInfo } from '~/types/user'
 
 /**
@@ -6,7 +6,7 @@ import type { UserInfo } from '~/types/user'
  * @param username
  * @param params
  */
-export function getUserInfo(username: string, params?: FetchOptions) {
+export function getUserInfo(username: string, params?: SearchParameters) {
   return request<UserInfo>(`/github/users/${username}`, {
     method: 'get',
     params,

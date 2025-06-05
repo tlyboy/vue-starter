598350af12606336ddf34628ecf5157d5d22321e
Guany <1084072690@qq.com>
2025-06-05 13:37:47 +0800

    feat: 调整默认布局，修改 DarkToggle 组件的位置并增加文本样式

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,15 +2,15 @@
   <div class="flex h-full flex-col items-center justify-center gap-4">
     <RouterView />
 
-    <div class="flex items-center gap-2">
-      <DarkToggle />
-
+    <div class="flex items-center gap-2 text-xl">
       <a
         class="i-carbon-logo-github icon-btn"
         href="https://github.com/tlyboy/vue-starter"
         target="_blank"
         rel="noopener noreferrer"
       ></a>
+
+      <DarkToggle />
     </div>
   </div>
 </template>
